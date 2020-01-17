d7443a5e72a16b0e7ea6be483ba3a31a522367c5
David Bundgaard <dapoman@gmail.com>
2020-01-17 18:39:01 +0100

    Updated with mandelbrot.js

---
+++ static/mandelbrot.js
@@ -0,0 +1,9 @@
+// mandelbrot computation and send back answer for each row...
+function scale(m, rmin, rmax, tmin, tmax) {
+    return ( (m - rmin) / (rmax - rmin) ) * (tmax - tmin) + tmin;
+}
+
+function calculate(pixelX, pixelY) {
+    let x0;
+    return 0;
+}
