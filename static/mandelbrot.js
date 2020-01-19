84a1c48a73521f599218a8d73e4338ed9c088a4d
David Bundgaard <dapoman@gmail.com>
2020-01-19 15:14:11 +0100

    Update with more files

--- static/mandelbrot.js
+++ static/mandelbrot.js
@@ -23,8 +23,8 @@ function calculate(x0, y0, maxIteration) {
 }
 
 
-function mandelbrot(xmin, xmax, ymin, ymax, width, height, result, iterations) {
-    let buffer = new Uint8Array(result)
+function mandelbrot(xmin, xmax, ymin, ymax, width, height, iterations) {
+    let buffer = new Uint8Array(width * height * 4)
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
 
@@ -41,17 +41,17 @@ function mandelbrot(xmin, xmax, ymin, ymax, width, height, result, iterations) {
                 let c = 3 * Math.log(iteration) / Math.log(iterations - 1.0);
                 if (c < 1) {
                     buffer[pos] = parseInt(104 * c);
-                    buffer[pos + 1] = 50
+                    buffer[pos + 1] = 100
                     buffer[pos + 2] = 190
                     //  buffer[pos + 1] = 128;
                     //  buffer[pos + 2] = 64;
                 } else if (c < 2) {
                     buffer[pos] = 104;
-                    buffer[pos + 1] = parseInt(50 * (c - 1));
-                  /*   buffer[pos + 2] = 190 */
+                    buffer[pos + 1] = parseInt(100 * (c - 1));
+                    /*   buffer[pos + 2] = 190 */
                 } else {
                     buffer[pos] = 104;
-                    buffer[pos + 1] = 50;
+                    buffer[pos + 1] = 100;
                     buffer[pos + 2] = parseInt(190 * (c - 2));
                 }
             }
@@ -65,13 +65,13 @@ onmessage = (evt) => {
     let data = evt.data;
     switch (data.type) {
         case "mandelbrot":
-            let result = new Uint8Array(mandelbrot(data.xmin, data.xmax, data.ymin, data.ymax, data.width, data.height, data.buffer, data.iterations));
+            let then = performance.now();
+            let result = new Uint8Array(mandelbrot(data.xmin, data.xmax, data.ymin, data.ymax, data.width, data.height, data.iterations));
+            let elapsed = performance.now() - then;
+            console.log(elapsed);
             postMessage({ buffer: result.buffer, status: "done", type: "mandelbrot" }, [result.buffer])
             break;
         default:
             console.log("dont understand message");
-    }
-    if (data.type === "mandelbrot") {
-
     }
 };
--- wasm/mandelbrot/mandelbrot.go
+++ wasm/mandelbrot/mandelbrot.go
@@ -1,13 +1,29 @@
 package main
 
-import "fmt"
-
 // mandelbrot wasm code
-func mandelbrot(width, height uint64) {
-	
+func mandelbrot(x0, y0 float64, maxIterations uint32) uint32 {
+	var (
+		a  float64
+		b  float64
+		rx float64
+		ry float64
+		ab float64
+	)
+
+	iterations := uint32(0)
+
+	for iterations < maxIterations && rx*rx+ry*ry <= 4 {
+		rx = a*a - b*b + x0
+		ab = a * b
+		ry = ab + ab + y0
+		a = rx
+		b = ry
+		iterations++
+	}
+	return iterations
+
 }
 
-
 func main() {
-	fmt.Println("WASM - Mandelbrot (c)")
+
 }
